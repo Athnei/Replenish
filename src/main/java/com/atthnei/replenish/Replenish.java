e65ade433a83bd9e122f512370d22f26b8bacb52
Athnei <enormousrobie@gmail.com>
2021-02-22 20:48:03 +0100

    checking for harmful food

--- src/main/java/com/atthnei/replenish/Replenish.java
+++ src/main/java/com/atthnei/replenish/Replenish.java
@@ -1,14 +1,21 @@
 package com.atthnei.replenish;
 
-import org.lwjgl.glfw.GLFW;
-import net.minecraft.item.ItemStack;
+import com.mojang.datafixers.util.Pair;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
+import java.util.Optional;
 
 public class Replenish implements ModInitializer {
 
@@ -22,8 +29,8 @@ public class Replenish implements ModInitializer {
             MOD_ID + ".key.categories"));
 
     private boolean wasKeySet = false;
-    private int slotIndexBeforePress = 0;
 
+    private int slotIndexBeforePress = 0;
 
     @Override
     public void onInitialize() {
@@ -31,7 +38,6 @@ public class Replenish implements ModInitializer {
     }
 
     private void onEndTick(MinecraftClient client) {
-
         if (keyBinding.isPressed()) {
 
             if (!wasKeySet) {
@@ -45,7 +51,6 @@ public class Replenish implements ModInitializer {
                     wasKeySet = true;
                 }
             }
-
         } else if (wasKeySet) {
             client.options.keyUse.setPressed(false);
             wasKeySet = false;
@@ -54,15 +59,33 @@ public class Replenish implements ModInitializer {
     }
 
     private int GetInventoryFoodIndex(ClientPlayerEntity player) {
-
         for (int i = 0; i < 9; i++) {
             ItemStack itemSlot = player.inventory.getStack(i);
 
             if (itemSlot.isFood()) {
-                return i;
+                boolean isHarmful = IsHarmful(itemSlot);
+
+                if (!isHarmful) {
+                    return i;
+                }
             }
         }
 
         return -1;
+    }
+
+    private boolean IsHarmful(ItemStack itemSlot) {
+        List<Pair<StatusEffectInstance, Float>> statusEffects = itemSlot.getItem().getFoodComponent().getStatusEffects();
+        Optional<Pair<StatusEffectInstance, Float>> first = statusEffects.stream().findFirst();
+
+        if (first.isPresent()) {
+            StatusEffect status = first.get().getFirst().getEffectType();
+
+            boolean ko = StatusEffects.HUNGER.getTranslationKey() == status.getTranslationKey();
+
+            return !status.isBeneficial();
+        }
+
+        return false;
     }
 }
