887feb70cf6d854b6fb57847968ff9c4466e87b1
Athnei <45879411+Athnei@users.noreply.github.com>
2021-06-19 21:49:18 +0200

    refactoring + updating to 1.17

--- src/main/java/com/atthnei/replenish/config/ModMenuIntegration.java
+++ src/main/java/com/atthnei/replenish/config/ModMenuIntegration.java
@@ -1,14 +1,10 @@
 package com.atthnei.replenish.config;
 
-//import io.github.prospector.modmenu.api.ConfigScreenFactory;
-//import io.github.prospector.modmenu.api.ModMenuApi;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
-import java.util.Map;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
@@ -16,10 +12,5 @@ public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> AutoConfig.getConfigScreen(ReplenishConfig.class, parent).get();
-    }
-
-    @Override
-    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-        return ModMenuApi.super.getProvidedConfigScreenFactories();
     }
 }
